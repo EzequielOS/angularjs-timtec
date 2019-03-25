a25e5c87fd7d2e7f1377f26d7ebb4a50cf039ec3
Ezequiel Oliveira <ezequiel.santana@sankhya.com.br>
2019-03-25 17:26:43 -0300

    Implementa filtros monetario de data e ordenacao por nome e idade

--- assets/js/primeiroController.js
+++ assets/js/primeiroController.js
@@ -1,24 +1,44 @@
-angular.module('minhaApp').controller('primeiroController', function($scope){
+angular.module('minhaApp').controller('primeiroController', function ($scope, $filter) {
     $scope.nome = 'Ezequiel Oliveira';
     var nome = "ashuashu";
 
-    $scope.iniciado = true;
-    $scope.alunos = ['Aluno 1', 'Aluno 2'];
+    $scope.hoje = new Date();
 
-    $scope.finalizar = function(){
+    $scope.iniciado = true;
+
+    $scope.alunos = [{ 'nome': 'Joãozinho', 'idade': 9 },
+    { 'nome': 'Ricardinho', 'idade': 11 },
+    { 'nome': 'Felipinho', 'idade': 11 },
+    { 'nome': 'Zildinha', 'idade': 14 },
+    { 'nome': 'Marianinha', 'idade': 10 },
+    { 'nome': 'Luluzinha', 'idade': 12 }];
+
+    $scope.finalizar = function () {
         $scope.iniciado = false;
     };
 
-    $scope.iniciar = function(){
+    $scope.iniciar = function () {
         $scope.iniciado = true;
     };
 
-    $scope.submeter = function(){
-        if($scope.form_1.$valid){
-            $scope.alunos.push($scope.nome_aluno);
-        }else{
-            alert("Prencha o nome corretamente! (Com mais de 3 caracteres!)");
+    $scope.submeter = function () {
+        if ($scope.form_1.$valid) {
+            novo_aluno = {};
+            novo_aluno['nome'] = $scope.nome_aluno;
+            novo_aluno['idade'] = parseInt($scope.idade_aluno);
+            $scope.alunos.push(novo_aluno);
+        } else {
+            alert("Preencha o formulário corretamente");
         }
-        
+    };
+
+    $scope.ordenarPorNome = function () {
+        $scope.ordenarPorNome = !$scope.ordenarPorNome;
+        $scope.alunos = $filter('orderBy')($scope.alunos, 'nome', $scope.ordenarPorNome);
+    };
+
+     $scope.ordenarPorIdade = function () {
+        $scope.ordenarPorIdade = !$scope.ordenarPorIdade;
+        $scope.alunos = $filter('orderBy')($scope.alunos, 'idade', $scope.ordenarPorIdade);
     };
 });
