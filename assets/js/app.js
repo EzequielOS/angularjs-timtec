e4cf6e600f44413eb714bb70be2dbfe896842673
Ezequiel Oliveira <ezequiel.santana@sankhya.com.br>
2019-03-25 18:13:48 -0300

    Implenta o uso de resource (Usando API de clima)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,1 @@
-var app = angular.module('minhaApp', []);
+var app = angular.module('minhaApp', ['ngResource']);
---
+++ assets/js/previsaoTempoController.js
@@ -0,0 +1,12 @@
+angular.module('minhaApp').controller('previsaoTempoController', function ($scope, $resource) {
+    var weatherChannel = $resource('https://api.openweathermap.org/data/2.5/weather?id=3445831&units=metric&APPID=cb736063c590f2abe550a94e94645939');
+    var getWeather = function () {
+        weatherChannel.get().$promise.then(function(response){
+            $scope.tempo = response;
+            console.log(response);
+        }, function () {
+            alert("Erro ao requisitar!");
+        })
+    }
+    getWeather();
+});
